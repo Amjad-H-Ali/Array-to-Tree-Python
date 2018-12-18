29b109c4544007413407f0195faea4ca1dcc30f4
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-12-17 23:14:14 -0600

    Function returns Null for values that do not exist

--- index.py
+++ index.py
@@ -3,4 +3,9 @@ print("Array To Tree!")
 # Function takes in an array and returns a tree
 
 def array_to_tree(array, index=0):
-	
+
+	# If index out of range (exit code for reccursion)
+	if index > len(array):
+		return None
+
+		
